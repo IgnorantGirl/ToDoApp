4389337deaf0934167649c30137bed697cd2e9e0
hui wang <heretoforeu@163.com>
2020-08-25 23:42:32 +0800

    add displayMultiConverter class

--- ViewModel/MainViewModel.cs
+++ ViewModel/MainViewModel.cs
@@ -15,10 +15,12 @@ namespace ToDoApp.ViewModel
         {
             MenuModels = new ObservableCollection<MenuModel>();
             menuModels.Add(new MenuModel() { IconFont = "\xe635", Title = "我的一天", BackColor = "#218868"  ,Count = 5 , Display = false});
-            menuModels.Add(new MenuModel() { IconFont = "\xe6b6", Title = "重要", BackColor = "#EE3B3B", Count = 5 });
-            menuModels.Add(new MenuModel() { IconFont = "\xe6e1", Title = "已计划日程", BackColor = "#218868", Count = 5 });
-            menuModels.Add(new MenuModel() { IconFont = "\xe614", Title = "已分配给我", BackColor = "#EE3B3B", Count = 5 });
-            menuModels.Add(new MenuModel() { IconFont = "\xe755", Title = "任务", BackColor = "#218868", Count = 5 });
+            menuModels.Add(new MenuModel() { IconFont = "\xe6b6", Title = "重要", BackColor = "#f58f98", Count = 5 });
+            menuModels.Add(new MenuModel() { IconFont = "\xe6e1", Title = "已计划日程", BackColor = "#84bf96", Count = 5 });
+            menuModels.Add(new MenuModel() { IconFont = "\xe614", Title = "已分配给我", BackColor = "#f8aba6", Count = 5 });
+            menuModels.Add(new MenuModel() { IconFont = "\xe755", Title = "任务", BackColor = "#50b7c1", Count = 5 });
+            //设置默认项
+            MenuModel = MenuModels[0];
 
             //初始化命令
             SelectedCommand = new RelayCommand<MenuModel>(t =>Select(t));
